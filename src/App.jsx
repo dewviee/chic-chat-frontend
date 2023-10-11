bd2f7ef1bc51a1149a2553bf9f29bf5753e21e69
Nutchanock Wisuttiphun <bell.blacksheep@gmail.com>
2023-10-12 01:09:21 +0700

    create chat page and login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import Login from './Auth/login.component';
-import Chat from './Chat'
 import { Routes ,Route } from 'react-router-dom';
-import Home from './Home/home.component';
 import Register from './Auth/Register/register.component';
+import Login from './Auth/Login/login.component';
+import Chat from './Chat/chat.component';
 
 function App() {
   return (
     <Routes>
       <Route path='/register' element={<Register />} />
-      
       <Route path='/room/:roomID' element={<Chat />} />
       <Route path='/login' element={<Login />} />
     </Routes>
