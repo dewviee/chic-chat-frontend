64f9842fa879534371634d819bbc1a50f90757fe
Nutchanock Wisuttiphun <bell.blacksheep@gmail.com>
2023-10-14 18:48:53 +0700

    Edit picture in home page

--- src/Components/Home/home.component.jsx
+++ src/Components/Home/home.component.jsx
@@ -8,10 +8,10 @@ const Home = () => {
         <p className="mt-4 ml-2 font-['Inter'] font-semibold text-[22px] text-white">ChicChat</p>
       </div>
 
-      <div className="flex flex-col flex-1 justify-center items-center mb-10">
-        <img src="/assets/images/homePic.png" alt="HomePicture" />
+      <div className="flex flex-col flex-1 justify-center items-center mb-20">
+        <img className="w-[20rem]" src="/assets/images/homePic.png" alt="HomePicture" />
 
-        <div className="flex flex-row mt-6">
+        <div className="flex flex-row">
           <div>
             <input
               className="w-full sm:w-[30rem] md:w-[50rem] h-16 pl-5 font-['Inter'] text-2xl
@@ -33,7 +33,7 @@ const Home = () => {
         </div>
 
         <div>
-          <button className="font-['Inter'] text-white h-16 py-3 px-16 mt-6
+          <button className="font-['Inter'] text-white h-16 py-3 px-16 mt-10
             rounded-full bg-gradient-to-r from-orange to-pink mx-8"
             type="button">CONFIRM</button>
         </div>
