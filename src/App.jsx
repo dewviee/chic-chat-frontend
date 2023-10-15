48648f063da318464e3b5a1cca9421d8bb3e9ad7
Nutchanock Wisuttiphun <bell.blacksheep@gmail.com>
2023-10-15 21:32:59 +0700

    Create create room page and edit text in register page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes ,Route } from 'react-router-dom';
 import Login from './Components/Auth/Login/login.component';
 import Register from './Components/Auth/Register/register.component';
 import Home from './Components/Home/home.component';
+import CreateRoom from './Components/Home/create-room.component';
 import Chat from './Components/Chat/chat.component';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route path='/register' element={<Register />} />
 
       <Route path='/' element={<Home />} />
+      <Route path='/createroom' element={<CreateRoom />} />
       <Route path='/room/:roomID' element={<Chat />} />
     </Routes>
   )
